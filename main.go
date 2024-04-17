74375b229488239c34c39aa3793ea06ca8e1007a
cgallagh <cgallagh@flextrade.com>
2024-04-17 11:06:10 -0500

    add slices

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 	conferenceName := "Go Conference"
 	const conferenceTickets = 50
 	var remainingTickets uint = 50
+	var bookings []string
 
 	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
@@ -16,8 +17,9 @@ func main() {
 	var firstName string
 	var lastName string
 	var email string
-	var userTickets int
-	// ask user for their name
+	var userTickets uint
+
+	// ask user input
 	fmt.Print("Enter you first name: ")
 	fmt.Scan(&firstName)
 
@@ -30,5 +32,16 @@ func main() {
 	fmt.Print("Enter number of tickets: ")
 	fmt.Scan(&userTickets)
 
+	remainingTickets = remainingTickets - userTickets
+	bookings = append(bookings, firstName+" "+lastName)
+
+	fmt.Printf("The whole slice: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Slice type %T\n", bookings)
+	fmt.Printf("Slice length %v\n", len(bookings))
+
 	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
+
+	fmt.Printf("These are all of our bookings: %v\n", bookings)
 }
