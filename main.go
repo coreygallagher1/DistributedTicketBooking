e7a9f5b44d6c9d965e0b4718978e8dfea213d388
cgallagh <cgallagh@flextrade.com>
2024-04-17 10:48:46 -0500

    Add basic IO

--- main.go
+++ main.go
@@ -3,12 +3,32 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
+	conferenceName := "Go Conference"
 	const conferenceTickets = 50
-	var remainingTickets = 50
+	var remainingTickets uint = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+	// ask user for their name
+	fmt.Print("Enter you first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Print("Enter you last name: ")
+	fmt.Scan(&lastName)
+
+	fmt.Print("Enter you email address: ")
+	fmt.Scan(&email)
+
+	fmt.Print("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
 }
